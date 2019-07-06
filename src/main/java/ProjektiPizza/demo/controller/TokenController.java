9c8a975a65a9a3922a55e49a56211796db89c8af
Drilon <ds37327@ubt-uni.net>
2019-07-06 21:37:43 +0200

    Adding JWT Security

---
+++ src/main/java/ProjektiPizza/demo/controller/TokenController.java
@@ -0,0 +1,27 @@
+package ProjektiPizza.demo.controller;
+
+import ProjektiPizza.demo.entity.User;
+import ProjektiPizza.demo.security.JwtGenerator;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+
+    private JwtGenerator jwtGenerator;
+
+    public TokenController(JwtGenerator jwtGenerator) {
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping
+    public String generate(@RequestBody final User user) {
+
+        return jwtGenerator.generate(user);
+
+    }
+}
